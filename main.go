fc684b62f3e896fcd41e69c20984b44ab6816471
haccht <haccht@users.noreply.github.com>
2025-04-30 17:06:07 +0900

    Refactor

--- main.go
+++ main.go
@@ -12,6 +12,53 @@ import (
 	flags "github.com/jessevdk/go-flags"
 )
 
+const helpText = `
+Format Examples:
+  ANSIC       "Mon Jan _2 15:04:05 2006"
+  UnixDate    "Mon Jan _2 15:04:05 MST 2006"
+  RubyDate    "Mon Jan 02 15:04:05 -0700 2006"
+  RFC822      "02 Jan 06 15:04 MST"
+  RFC822Z     "02 Jan 06 15:04 -0700"
+  RFC850      "Monday, 02-Jan-06 15:04:05 MST"
+  RFC1123     "Mon, 02 Jan 2006 15:04:05 MST"
+  RFC1123Z    "Mon, 02 Jan 2006 15:04:05 -0700"
+  RFC3339     "2006-01-02T15:04:05Z07:00"
+  RFC3339Nano "2006-01-02T15:04:05.999999999Z07:00"
+  Kitchen     "3:04PM"
+  Stamp       "Jan _2 15:04:05"
+  StampMilli  "Jan _2 15:04:05.000"
+  StampMicro  "Jan _2 15:04:05.000000"
+  StampNano   "Jan _2 15:04:05.000000000"
+  DateTime    "2006-01-02 15:04:05"
+  DateOnly    "2006-01-02"
+  TimeOnly    "15:04:05"
+  Unix        "1136239445"
+  Unix-Milli  "1136239445000"
+  Unix-Micro  "1136239445000000"
+
+  Arbitrary formats are also supported. See https://pkg.go.dev/time as a reference.`
+
+var layouts = map[string]string{
+	"ansic": time.ANSIC,
+	"unixdate": time.UnixDate,
+	"rubydate": time.RubyDate,
+	"rfc822": time.RFC822,
+	"rfc822z": time.RFC822Z,
+	"rfc850": time.RFC850,
+	"rfc1123": time.RFC1123,
+	"rfc1123z": time.RFC1123Z,
+	"rfc3339": time.RFC3339,
+	"rfc3339nano": time.RFC3339Nano,
+	"kitchen": time.Kitchen,
+	"stamp": time.Stamp,
+	"stampmilli": time.StampMilli,
+	"stampmicro": time.StampMicro,
+	"stampnano": time.StampNano,
+	"datetime": time.DateTime,
+	"dateonly": time.DateOnly,
+	"timeonly": time.TimeOnly,
+}
+
 type options struct {
 	In   string `short:"i" long:"in" description:"Specify input time format" default:"RFC3339"`
 	Out  string `short:"o" long:"out" description:"Specify output time format" default:"RFC3339"`
@@ -30,37 +77,43 @@ func getScanner(args []string) *bufio.Scanner {
 	return bufio.NewScanner(os.Stdin)
 }
 
-func parseValue(format, value string) (time.Time, error) {
-	layout := loadLayout(format)
+func parseValue(v, format string) (time.Time, error) {
+	layout, ok := layouts[format]
+	if ok {
+		return time.Parse(layout, v)
+	}
 
-	switch strings.ToLower(layout) {
+	switch layout {
 	case "unix":
-		f, err := strconv.ParseFloat(value, 64)
+		f, err := strconv.ParseFloat(v, 64)
 		if err != nil {
-			return time.Time{}, fmt.Errorf("failed to parse epoch time: %s", value)
+			return time.Time{}, fmt.Errorf("failed to parse epoch time: %s", v)
 		}
 		return time.UnixMicro(int64(f * 1000000)), nil
 	case "unix.milli":
-		f, err := strconv.ParseFloat(value, 64)
+		f, err := strconv.ParseFloat(v, 64)
 		if err != nil {
-			return time.Time{}, fmt.Errorf("failed to parse epoch time: %s", value)
+			return time.Time{}, fmt.Errorf("failed to parse epoch time: %s", v)
 		}
 		return time.UnixMicro(int64(f * 1000)), nil
 	case "unix.micro":
-		f, err := strconv.ParseFloat(value, 64)
+		f, err := strconv.ParseFloat(v, 64)
 		if err != nil {
-			return time.Time{}, fmt.Errorf("failed to parse epoch time: %s", value)
+			return time.Time{}, fmt.Errorf("failed to parse epoch time: %s", v)
 		}
 		return time.UnixMicro(int64(f)), nil
 	default:
-		return time.Parse(layout, value)
+		return time.Time{}, fmt.Errorf("failed to parse time: %s", v)
 	}
 }
 
-func formatValue(format string, t time.Time) string {
-	layout := loadLayout(format)
+func formatValue(t time.Time, format string) string {
+	layout, ok := layouts[format]
+	if ok {
+		return t.Format(layout)
+	}
 
-	switch strings.ToLower(layout) {
+	switch layout {
 	case "unix":
 		f := float64(t.UnixNano()) / 1000000000
 		return strconv.FormatFloat(f, 'f', -1, 64)
@@ -70,49 +123,6 @@ func formatValue(format string, t time.Time) string {
 	case "unix.micro":
 		f := float64(t.UnixNano()) / 1000
 		return strconv.FormatFloat(f, 'f', -1, 64)
-	default:
-		return t.Format(layout)
-	}
-}
-
-func loadLayout(format string) string {
-	switch strings.ToLower(format) {
-	case "ansic":
-		return time.ANSIC
-	case "unixdate":
-		return time.UnixDate
-	case "rubydate":
-		return time.RubyDate
-	case "rfc822":
-		return time.RFC822
-	case "rfc822z":
-		return time.RFC822Z
-	case "rfc850":
-		return time.RFC850
-	case "rfc1123":
-		return time.RFC1123
-	case "rfc1123z":
-		return time.RFC1123Z
-	case "rfc3339":
-		return time.RFC3339
-	case "rfc3339nano":
-		return time.RFC3339Nano
-	case "kitchen":
-		return time.Kitchen
-	case "stamp":
-		return time.Stamp
-	case "stampmilli":
-		return time.StampMilli
-	case "stampmicro":
-		return time.StampMicro
-	case "stampnano":
-		return time.StampNano
-	case "datetime":
-		return time.DateTime
-	case "dateonly":
-		return time.DateOnly
-	case "timeonly":
-		return time.TimeOnly
 	default:
 		return format
 	}
@@ -142,35 +152,14 @@ func run() error {
 		var message bytes.Buffer
 
 		parser.WriteHelp(&message)
-		fmt.Fprint(&message, `
-Format Examples:
-  ANSIC       "Mon Jan _2 15:04:05 2006"
-  UnixDate    "Mon Jan _2 15:04:05 MST 2006"
-  RubyDate    "Mon Jan 02 15:04:05 -0700 2006"
-  RFC822      "02 Jan 06 15:04 MST"
-  RFC822Z     "02 Jan 06 15:04 -0700"
-  RFC850      "Monday, 02-Jan-06 15:04:05 MST"
-  RFC1123     "Mon, 02 Jan 2006 15:04:05 MST"
-  RFC1123Z    "Mon, 02 Jan 2006 15:04:05 -0700"
-  RFC3339     "2006-01-02T15:04:05Z07:00"
-  RFC3339Nano "2006-01-02T15:04:05.999999999Z07:00"
-  Kitchen     "3:04PM"
-  Stamp       "Jan _2 15:04:05"
-  StampMilli  "Jan _2 15:04:05.000"
-  StampMicro  "Jan _2 15:04:05.000000"
-  StampNano   "Jan _2 15:04:05.000000000"
-  DateTime    "2006-01-02 15:04:05"
-  DateOnly    "2006-01-02"
-  TimeOnly    "15:04:05"
-  Unix        "1136239445"
-  Unix-Milli  "1136239445000"
-  Unix-Micro  "1136239445000000"
-
-  Arbitrary formats are also supported. See https://pkg.go.dev/time as a reference.`)
+		fmt.Fprint(&message, helpText)
 
 		fmt.Println(message.String())
 		os.Exit(0)
 	}
+
+	opts.In = strings.ToLower(opts.In)
+	opts.Out = strings.ToLower(opts.Out)
 
 	loc := time.Local
 	if opts.Tz != "" {
@@ -182,31 +171,34 @@ Format Examples:
 		loc = l
 	}
 
-	times := []time.Time{}
 	if opts.Now {
 		t := time.Now()
-		times = append(times, t)
+
+		t = t.Add(parseDuration(opts.Add))
+		t = t.Add(parseDuration(opts.Sub) * -1)
+		t = t.In(loc)
+		fmt.Println(formatValue(t, opts.Out))
 	} else {
 		scanner := getScanner(args)
+		w := bufio.NewWriter(os.Stdout)
+		defer w.Flush()
+
 		for scanner.Scan() {
 			v := strings.TrimSpace(scanner.Text())
-			t, err := parseValue(opts.In, v)
-			if err == nil {
-				times = append(times, t)
+			t, err := parseValue(v, opts.In)
+			if err != nil {
+				return err
 			}
+
+			t = t.Add(parseDuration(opts.Add))
+			t = t.Add(parseDuration(opts.Sub) * -1)
+			t = t.In(loc)
+			fmt.Println(formatValue(t, opts.Out))
 		}
 
 		if err := scanner.Err(); err != nil {
 			return scanner.Err()
 		}
-	}
-
-	for _, t := range times {
-		t = t.In(loc)
-		t = t.Add(parseDuration(opts.Add))
-		t = t.Add(parseDuration(opts.Sub) * -1)
-
-		fmt.Println(formatValue(opts.Out, t))
 	}
 
 	return nil
