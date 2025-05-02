c97498b074bcad5582b1398e14cba9d69e0c68d1
haccht <haccht@users.noreply.github.com>
2025-05-02 10:43:44 +0900

    Add feature to search pattern and convert

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
-	"regexp"
 
 	flags "github.com/jessevdk/go-flags"
 )
@@ -39,39 +39,39 @@ const helpText = `Format Examples:
   Arbitrary formats are also supported. See https://pkg.go.dev/time as a reference.`
 
 var layouts = map[string]string{
-	"ansic": time.ANSIC,
-	"unixdate": time.UnixDate,
-	"rubydate": time.RubyDate,
-	"rfc822": time.RFC822,
-	"rfc822z": time.RFC822Z,
-	"rfc850": time.RFC850,
-	"rfc1123": time.RFC1123,
-	"rfc1123z": time.RFC1123Z,
-	"rfc3339": time.RFC3339,
+	"ansic":       time.ANSIC,
+	"unixdate":    time.UnixDate,
+	"rubydate":    time.RubyDate,
+	"rfc822":      time.RFC822,
+	"rfc822z":     time.RFC822Z,
+	"rfc850":      time.RFC850,
+	"rfc1123":     time.RFC1123,
+	"rfc1123z":    time.RFC1123Z,
+	"rfc3339":     time.RFC3339,
 	"rfc3339nano": time.RFC3339Nano,
-	"kitchen": time.Kitchen,
-	"stamp": time.Stamp,
-	"stampmilli": time.StampMilli,
-	"stampmicro": time.StampMicro,
-	"stampnano": time.StampNano,
-	"datetime": time.DateTime,
-	"dateonly": time.DateOnly,
-	"timeonly": time.TimeOnly,
+	"kitchen":     time.Kitchen,
+	"stamp":       time.Stamp,
+	"stampmilli":  time.StampMilli,
+	"stampmicro":  time.StampMicro,
+	"stampnano":   time.StampNano,
+	"datetime":    time.DateTime,
+	"dateonly":    time.DateOnly,
+	"timeonly":    time.TimeOnly,
 }
 
 type options struct {
-	In   string `short:"i" long:"in" description:"Specify input time format (default: guess format)"`
-	Out  string `short:"o" long:"out" description:"Specify output time format" default:"rfc3339"`
-	Now  bool   `short:"n" long:"now" description:"Load currnet time as input"`
-	Add  string `short:"a" long:"add" description:"Append specified duration (ex. 5m, 1.5h, 1h30m)"`
-	Sub  string `short:"s" long:"sub" description:"Substract specified duration (ex. 5m, 1.5h, 1h30m)"`
-	Tz   string `short:"z" long:"tz" description:"Override timezone"`
-	Help bool   `short:"h" long:"help" description:"Show this help message"`
+	In      string    `short:"i" long:"in" description:"Specify input time format (default: guess format)"`
+	Out     string    `short:"o" long:"out" description:"Specify output time format" default:"rfc3339"`
+	Now     bool      `short:"n" long:"now" description:"Load currnet time as input"`
+	Add     string    `short:"a" long:"add" description:"Append specified duration (ex. 5m, 1.5h, 1h30m)"`
+	Sub     string    `short:"s" long:"sub" description:"Substract specified duration (ex. 5m, 1.5h, 1h30m)"`
+	Loc      string `short:"z" long:"loc" description:"Override timezone"`
+	Pattern string   `short:"g" long:"grep" description:"Replace strings that match the regular expression"`
+	Help    bool      `short:"h" long:"help" description:"Show this help message"`
 }
 
-
 type guessRule struct {
-	re *regexp.Regexp
+	re      *regexp.Regexp
 	layouts []string
 }
 
@@ -84,7 +84,7 @@ var guessRules = []guessRule{
 	{regexp.MustCompile(`\d{1,2}:\d{2}(AM|PM)`), []string{"kitchen"}},
 }
 
-func getScanner(args []string) *bufio.Scanner {
+func genScanner(args []string) *bufio.Scanner {
 	if len(args) > 0 {
 		reader := strings.NewReader(strings.Join(args, "\n"))
 		return bufio.NewScanner(reader)
@@ -92,20 +92,8 @@ func getScanner(args []string) *bufio.Scanner {
 	return bufio.NewScanner(os.Stdin)
 }
 
-func guessTime(s string) (time.Time, error) {
-	for _, rule := range guessRules {
-		if rule.re.MatchString(s) {
-			for _, l := range rule.layouts {
-				if t, err := toTime(s, l); err == nil {
-					return t, nil
-				}
-			}
-		}
-	}
-	return time.Time{}, fmt.Errorf("Unknown format: %s", s)
-}
-
-func toTime(s, format string) (time.Time, error) {
+func stringToTime(s, f string) (time.Time, error) {
+	format := strings.ToLower(f)
 	if format == "" {
 		return guessTime(s)
 	}
@@ -120,13 +108,13 @@ func toTime(s, format string) (time.Time, error) {
 			return time.Time{}, fmt.Errorf("failed to parse epoch time: %s", s)
 		}
 		return time.UnixMicro(int64(f * 1000000)), nil
-	case "unix.milli":
+	case "unix-milli":
 		f, err := strconv.ParseFloat(s, 64)
 		if err != nil {
 			return time.Time{}, fmt.Errorf("failed to parse epoch time: %s", s)
 		}
 		return time.UnixMicro(int64(f * 1000)), nil
-	case "unix.micro":
+	case "unix-micro":
 		f, err := strconv.ParseFloat(s, 64)
 		if err != nil {
 			return time.Time{}, fmt.Errorf("failed to parse epoch time: %s", s)
@@ -137,7 +125,8 @@ func toTime(s, format string) (time.Time, error) {
 	}
 }
 
-func toString(t time.Time, format string) string {
+func timeToString(t time.Time, f string) string {
+	format := strings.ToLower(f)
 	if layout, ok := layouts[format]; ok {
 		return t.Format(layout)
 	}
@@ -146,10 +135,10 @@ func toString(t time.Time, format string) string {
 	case "unix":
 		f := float64(t.UnixNano()) / 1000000000
 		return strconv.FormatFloat(f, 'f', -1, 64)
-	case "unix.milli":
+	case "unix-milli":
 		f := float64(t.UnixNano()) / 1000000
 		return strconv.FormatFloat(f, 'f', -1, 64)
-	case "unix.micro":
+	case "unix-micro":
 		f := float64(t.UnixNano()) / 1000
 		return strconv.FormatFloat(f, 'f', -1, 64)
 	default:
@@ -157,12 +146,31 @@ func toString(t time.Time, format string) string {
 	}
 }
 
+func guessTime(s string) (time.Time, error) {
+	for _, rule := range guessRules {
+		if rule.re.MatchString(s) {
+			for _, l := range rule.layouts {
+				if t, err := stringToTime(s, l); err == nil {
+					return t, nil
+				}
+			}
+		}
+	}
+	return time.Time{}, fmt.Errorf("Unknown format: %s", s)
+}
+
+func modifyTime(t time.Time, loc *time.Location, add, sub time.Duration) time.Time {
+	t = t.In(loc)
+	t = t.Add(add)
+	t = t.Add(sub * -1)
+	return t
+}
+
 func parseDuration(d string) time.Duration {
 	u, err := time.ParseDuration(d)
 	if err != nil {
 		return 0
 	}
-
 	return u
 }
 
@@ -170,57 +178,69 @@ func run() error {
 	var opts options
 	parser := flags.NewParser(&opts, flags.Default&^flags.HelpFlag)
 	parser.Usage = "[Options]"
-
 	args, err := parser.Parse()
 	if err != nil {
 		os.Exit(1)
 	}
+
 	if opts.Help {
 		var message bytes.Buffer
-
 		parser.WriteHelp(&message)
+		fmt.Fprint(&message, "")
 		fmt.Fprint(&message, helpText)
-
-		fmt.Println(message.String())
+		fmt.Fprintln(os.Stdout, message.String())
 		os.Exit(0)
 	}
 
-	opts.In = strings.ToLower(opts.In)
-	opts.Out = strings.ToLower(opts.Out)
+	add := parseDuration(opts.Add)
+	sub := parseDuration(opts.Sub)
 
 	loc := time.Local
-	if opts.Tz != "" {
-		l, err := time.LoadLocation(opts.Tz)
-		if err != nil {
+	if opts.Loc != "" {
+		if v, err := time.LoadLocation(opts.Loc); err != nil {
 			return err
+		} else {
+			loc = v
 		}
+	}
 
-		loc = l
+	var pat *regexp.Regexp
+	if opts.Pattern != "" {
+		if v, err := regexp.Compile(opts.Pattern); err != nil {
+			return err
+		} else {
+			pat = v
+		}
 	}
 
 	if opts.Now {
 		t := time.Now()
-
-		t = t.Add(parseDuration(opts.Add))
-		t = t.Add(parseDuration(opts.Sub) * -1)
-		t = t.In(loc)
-		fmt.Println(toString(t, opts.Out))
+		t = modifyTime(t, loc, add, sub)
+		fmt.Println(timeToString(t, opts.Out))
 	} else {
-		scanner := getScanner(args)
-		w := bufio.NewWriter(os.Stdout)
-		defer w.Flush()
-
+		scanner := genScanner(args)
 		for scanner.Scan() {
-			v := strings.TrimSpace(scanner.Text())
-			t, err := toTime(v, opts.In)
-			if err != nil {
-				return err
-			}
+			line := scanner.Text()
+			if opts.Pattern == "" {
+				t, err := stringToTime(strings.TrimSpace(line), opts.In)
+				if err != nil {
+					return err
+				}
 
-			t = t.Add(parseDuration(opts.Add))
-			t = t.Add(parseDuration(opts.Sub) * -1)
-			t = t.In(loc)
-			fmt.Println(toString(t, opts.Out))
+				t = modifyTime(t, loc, add, sub)
+				fmt.Println(timeToString(t, opts.Out))
+			} else {
+				replaced := pat.ReplaceAllStringFunc(line, func(s string) string {
+					t, err := stringToTime(s, opts.In)
+					if err != nil {
+						return s
+					}
+
+					t = modifyTime(t, loc, add, sub)
+					return timeToString(t, opts.Out)
+				})
+				fmt.Println(replaced)
+			}
 		}
 
 		if err := scanner.Err(); err != nil {
